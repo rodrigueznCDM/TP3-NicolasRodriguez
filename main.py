589a6b31e11527f59974f4ee443b722d22e4a178
rodrigueznCDM <rodriguezn@educ.college-montreal.qc.ca>
2024-10-02 10:45:14 -0400

    periode terminé #3

--- main.py
+++ main.py
@@ -13,83 +13,120 @@ past_battle = "aucun"
 consecutive_victories = 0
 victories = 0
 defeats = 0
-rules_seen = False
+skip_exploit_possible = False
+boss_encountered = False
+end_game = False
 
 
 def monster_generate():
-    global rules_seen
-    monster = random.randint(1, 5)
+    global skip_exploit_possible
+    global boss_encountered
+    # les monstres normaux seronts toujours plus faibles que le boss
+    normal_monster = random.randint(1, 4)
+    boss_monster = 5
 
-    if not rules_seen:
-        return monster
+    # empècher un "exploit" qui permet de contourner sans perdre de point de vie
+    if not skip_exploit_possible:
 
-    elif rules_seen:
-        rules_seen = False
-        return previous_monster
+        if boss_encountered:
+            return boss_monster
+
+        elif not boss_encountered:
+            return normal_monster
+
+    elif skip_exploit_possible:
+        skip_exploit_possible = False
+
+        if boss_encountered:
+            return boss_monster
+
+        elif not boss_encountered:
+            return previous_monster
 
 
-while hp > 0:
-    num_encounters += 1
-    difficulty = monster_generate()
-    previous_monster = difficulty
-    print(f"\nVous tombez face à face avec un adversaire d'une difficulté de {difficulty}\n")
-    battle = int(input(f"Que voulez-vous faire? :"
-                       f"\n1- Combattre cet adversaire"
-                       f"\n2- Contourner cet adversaire et aller ouvrir une autre porte"
-                       f"\n3- Afficher les règles du jeu"
-                       f"\n4- Quitter la partie\n\n"))
-    # message d'erreur
-    try:
-        if battle == 1:
-            dice = random.randint(1, 6)
-            print(f"\nStatu de la partie:"
-                  f"\n- Advesaire #{num_encounters}"
-                  f"\n- Adversaire avec {difficulty} de force"
-                  f"\n- Vous avez {hp} point(s) de vie"
-                  f"\n- Combat #{num_encounters}: {victories} victoires et {defeats} défaites"
-                  f"\n- Résultat de la dernière bataille: {past_battle}"
-                  f"\n- Résultat de votre lancer: {dice}\n")
+while not end_game:
 
-            if dice > difficulty:
-                victories += 1
-                consecutive_victories += 1
-                past_battle = "Victoire"
-                print(f"Victoire:"
+    while hp > 0:
+        num_encounters += 1
+        difficulty = 0
+
+        # on s'assure que le boss n'apparait qu'une fois
+        if victories == 3 and not boss_encountered:
+            boss_encountered = True
+            enemy = "le boss"
+            difficulty = monster_generate()
+            print(f"\nVous tombez face à face avec le boss. C'est un monstre d'une difficulté de {difficulty}\n")
+
+        else:
+            enemy = "cet adversaire"
+            difficulty = monster_generate()
+            print(f"\nVous tombez face à face avec un adversaire d'une difficulté de {difficulty}\n")
+            previous_monster = difficulty
+
+        # message d'erreur personnalisé
+        try:
+            battle = int(input(f"Que voulez-vous faire? :"
+                               f"\n1- Combattre {enemy}"
+                               f"\n2- Contourner {enemy} et aller ouvrir une autre porte"
+                               f"\n3- Afficher les règles du jeu"
+                               f"\n4- Quitter la partie\n\n"))
+            if battle == 1:
+                dice = random.randint(1, 6)
+                print(f"\nStatu de la partie:"
+                      f"\n- Advesaire #{num_encounters}"
+                      f"\n- Adversaire avec {difficulty} de force"
                       f"\n- Vous avez {hp} point(s) de vie"
-                      f"\n- Vous avez gagné {consecutive_victories} victoires consécutives\n")
+                      f"\n- Combat #{num_encounters}: {victories} victoires et {defeats} défaites"
+                      f"\n- Résultat de la dernière bataille: {past_battle}"
+                      f"\n- Résultat de votre lancer: {dice}\n")
 
-            elif dice <= difficulty:
-                defeats += 1
-                consecutive_victories = 0
-                hp -= difficulty
-                past_battle = "Défaite"
-                print(f"Défaite:"
-                      f"\n- Vous avez {hp} point(s) de vie\n")
+                if dice > difficulty:
+                    victories += 1
+                    consecutive_victories += 1
+                    past_battle = "Victoire"
+                    print(f"Victoire:"
+                          f"\n- Vous avez {hp} point(s) de vie"
+                          f"\n- Vous avez gagné {consecutive_victories} victoires consécutives\n")
 
-        elif battle == 2:
-            hp -= 1
-            print(f"\nVous perdez 1 point de vie en vous échapant du monstre."
-                  f"\nVous avez {hp} point(s) de vie\n")
+                elif dice <= difficulty:
+                    defeats += 1
+                    consecutive_victories = 0
+                    hp -= difficulty
+                    past_battle = "Défaite"
+                    print(f"Défaite:"
+                          f"\n- Vous avez {hp} point(s) de vie\n")
 
-        elif battle == 3:
-            rules_seen = True
-            print(f"\nPour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de"
-                  f" l’adversaire."
-                  f"\nDans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire."
-                  f"\nUne défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force"
-                  f" de l’adversaire."
-                  f"\nDans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.\n"
-                  f"\nLa partie se termine lorsque les points de vie de l’usager tombent sous 0.\n"
-                  f"\nL’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une"
-                  f" pénalité de 1 point de vie.\n")
+            elif battle == 2:
 
-        elif battle == 4:
-            exit("\nMerci et au revoir...")
+                # s'assurer que le joueur se bat contre le boss
+                if not boss_encountered:
+                    hp -= 1
+                    print(f"\nVous perdez 1 point de vie en vous échapant du monstre."
+                          f"\nVous avez {hp} point(s) de vie\n")
 
-    except ValueError:
-        print("\nEntrez un numéro.\n")
-        # on reutilise la variable
-        rules_seen = True
+                elif boss_encountered:
+                    # pour que le joueur rencontre le boss encore
+                    boss_encountered = False
+                    print("\nVous ne pouvez pas contourner le boss\n")
 
-print(f"\nVous êtes mort."
-      f"\nVous avez vaincu {victories} monstre(s) en total.")
+            elif battle == 3:
+                skip_exploit_possible = True
+                print("\nPour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de"
+                      " l’adversaire."
+                      "\nDans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire."
+                      "\nUne défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la"
+                      " force de l’adversaire."
+                      "\nDans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.\n"
+                      "\nLa partie se termine lorsque les points de vie de l’usager tombent sous 0.\n"
+                      "\nL’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a"
+                      " une pénalité d'un point de vie.\n")
+
+            elif battle == 4:
+                exit("\nMerci et au revoir...")
+
+        except ValueError:
+            print("\nEntrez un numéro.\n")
+            skip_exploit_possible = True
+
+    print(f"\nVous êtes mort."
+          f"\nVous avez vaincu {victories} monstre(s) en total.")
